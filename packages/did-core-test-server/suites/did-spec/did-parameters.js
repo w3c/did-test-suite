d4d005d6c3f4d1c9b2793cde52faa1ec0b3727ca
Charles E. Lehner <charles.lehner@spruceid.com>
2021-03-08 12:23:10 -0500

    Parse versionTime DID parameter

--- packages/did-core-test-server/suites/did-spec/did-parameters.js
+++ packages/did-core-test-server/suites/did-spec/did-parameters.js
@@ -1,5 +1,10 @@
 const utils = require('./utils');
 
+// https://www.w3.org/TR/xmlschema11-2/#nt-dateTimeRep
+// Constrained to UTC and without sub-second decimal precision.
+// Note: day-of-month constraints not applied.
+const versionTimeRe = /^(-?(?:[1-9][0-9]{3,})|(?:0[0-9]{3}))-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T((?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]|24:00:00)(?:\.0+)?(?:Z|[+-]00:00)$/;
+
 const didParametersTests = (suiteConfig) => {
   if (suiteConfig.didParameters) {
     describe('did-parameters', () => {
@@ -27,6 +32,19 @@ const didParametersTests = (suiteConfig) => {
               }
             });
           }
+
+          if (didParameter === 'versionTime') {
+            it('MUST be [an ASCII string which is] a valid XML datetime value, as defined in section 3.3.7 of W3C XML Schema Definition Language (XSD) 1.1 Part 2: Datatypes [XMLSCHEMA11-2]. This datetime value MUST be normalized to UTC 00:00:00 and without sub-second decimal precision. For example: 2020-12-20T19:17:47Z.', async () => {
+              const didUrl = suiteConfig.didParameters[didParameter];
+              const param = utils.getQueryParamValueFromDidUri(
+                didUrl,
+                'versionTime'
+              );
+
+              expect(versionTimeRe.test(param)).toBe(true);
+            });
+          }
+
         });
       });
     });
