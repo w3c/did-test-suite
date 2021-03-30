edef7b54c6c7af735da0504be148fa3f609c2f42
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-03-31 08:36:49 +0900

    7.3 Metadata Structure - complex recursive structure support

--- packages/did-core-test-server/suites/did-spec/did-metadata-structure.js
+++ packages/did-core-test-server/suites/did-spec/did-metadata-structure.js
@@ -1,3 +1,39 @@
+const didMetadataStructureTest_r = (didDocumentMetadata) => {
+    describe("didDocumentMetadata", () => {
+        it('The structure used to communicate this metadata MUST be a map of properties.', () => {
+            expect(didDocumentMetadata instanceof Object).toBeTruthy();
+        });
+
+        it('Each property name MUST be a string.', () => {
+            Object.keys(didDocumentMetadata).forEach((property_name) => {
+                expect(typeof property_name).toBe("string");
+            });
+        });
+
+        it('Each property value MUST be a string, map, list, ordered set, boolean, or null.', () => {
+            Object.keys(didDocumentMetadata).forEach((property_name) => {
+                expect(typeof didDocumentMetadata[property_name]).toEqual(expect.stringMatching(/(string|object|boolean)/));  // map, list, set and null are objects
+            });
+        });
+
+        it('The values within any complex data structures such as maps and lists ' +
+            'MUST be one of these data types as well.', () => {
+                Object.keys(didDocumentMetadata).forEach((property_name) => {
+                    if (typeof didDocumentMetadata[property_name] == "object") {
+                        didMetadataStructureTest_r(didDocumentMetadata[property_name]);
+                    }
+                });
+            });
+
+        it.todo('All metadata property definitions registered in the DID Specification Registries ' +
+            '[DID-SPEC-REGISTRIES] MUST define the value type, including any additional formats ' +
+            'or restrictions to that value (for example, a string formatted as a date or as a decimal integer).');
+
+        it.todo('The entire metadata structure MUST be serializable according to the JSON ' +
+            'serialization rules in the [INFRA] specification.');
+    });
+};
+
 const didMetadataStructureTests = (suiteConfig) => {
     describe('7.3 Metadata Structure', () => {
         suiteConfig.dids.forEach((didExample) => {
@@ -6,29 +42,7 @@ const didMetadataStructureTests = (suiteConfig) => {
                     describe(contentType, () => {
                         const didDocumentMetadata = suiteConfig[didExample][contentType].didDocumentMetadata;
                         if (didDocumentMetadata) {
-                            describe("didDocumentMetadata", () => {
-                                it('The structure used to communicate this metadata MUST be a map of properties.', () => {
-                                    expect(didDocumentMetadata instanceof Object).toBeTruthy();
-                                });
-
-                                it('Each property name MUST be a string.', () => {
-                                    Object.keys(didDocumentMetadata).forEach((property_name) => {
-                                        expect(typeof property_name).toBe("string");
-                                    });
-                                });
-
-                                it.todo('Each property value MUST be a string, map, list, ordered set, boolean, or null.');
-
-                                it.todo('The values within any complex data structures such as maps and lists ' +
-                                    'MUST be one of these data types as well.');
-
-                                it.todo('All metadata property definitions registered in the DID Specification Registries ' +
-                                    '[DID-SPEC-REGISTRIES] MUST define the value type, including any additional formats ' +
-                                    'or restrictions to that value (for example, a string formatted as a date or as a decimal integer).');
-
-                                it.todo('The entire metadata structure MUST be serializable according to the JSON ' +
-                                    'serialization rules in the [INFRA] specification.');
-                            });
+                            didMetadataStructureTest_r(didDocumentMetadata);
                         };
                     });
                 })
