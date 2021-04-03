264c450470107bfe3a5c0c2a723dbfdeadcbd402
Manu Sporny <msporny@digitalbazaar.com>
2021-04-03 15:47:07 -0400

    Remove old test suite layout.

--- packages/did-core-test-server/suites/did-spec/default.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "name": "did-spec",
-  "didMethods": [
-    require('../implementations/did-example-didwg.json'),
-    require('../implementations/did-key-2018-db.json')
-  ]
-}
