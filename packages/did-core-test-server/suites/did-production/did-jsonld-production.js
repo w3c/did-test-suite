75354e293f0ebd9e07b3e361ff2b1cb33926673b
Manu Sporny <msporny@digitalbazaar.com>
2021-04-03 15:46:36 -0400

    Create new did-production test suite.

---
+++ packages/did-core-test-server/suites/did-production/did-jsonld-production.js
@@ -0,0 +1,60 @@
+const deepEqual = require('deep-equal')
+
+const generateJsonldProductionTests = ({did, resolutionResult}) => {
+  const {didDocument} = resolutionResult;
+
+  it('6.3.1 JSON-LD Production - The DID document and any DID document ' +
+    'data structures expressed by the data model MUST be serialized to the ' +
+    'JSON-LD representation according to the JSON representation production ' +
+    'rules as defined in § 6.2 JSON.', async () => {
+      reserializedDidDocument = JSON.parse(JSON.stringify(didDocument));
+      expect(deepEqual(didDocument, reserializedDidDocument)).toBe(true);
+  });
+
+  it('6.3.1 JSON-LD Production - In addition to using the JSON  ' +
+    'representation production rules, JSON-LD production MUST include the ' +
+    'representation-specific @context entry.', async () => {
+      expect(didDocument).toHaveProperty('@context');
+  });
+
+  it('6.3.1 JSON-LD Production - The serialized value of @context ' +
+    'MUST be the JSON String https://www.w3.org/ns/did/v1, or a JSON ' +
+    'Array where the first item is the JSON String ' +
+    'https://www.w3.org/ns/did/v1 and the subsequent items are serialized ' +
+    'according to the JSON representation production rules.', async () => {
+      const context = didDocument['@context'];
+      if(typeof context === 'string') {
+        expect(context).toBe('https://www.w3.org/ns/did/v1');
+      } else if(Array.isArray(context)) {
+        expect(context[0]).toBe('https://www.w3.org/ns/did/v1');
+        reserializedContext = JSON.parse(JSON.stringify(context));
+        expect(deepEqual(context, reserializedContext)).toBe(true);
+      } else {
+        throw new Error('Invalid @context value '+ context);
+      }
+  });
+
+  it('6.3.1 JSON-LD Production - When serializing a JSON-LD ' +
+    'representation of a DID document, a conforming producer MUST specify a ' +
+    'media type of application/did+ld+json to downstream applications such ' +
+    'as described in § 7.1.2 DID Resolution Metadata.', async () => {
+      const {contentType} = resolutionResult.didResolutionMetadata;
+      expect(contentType).toBe('application/did+ld+json');
+  });
+}
+
+const didJsonldProductionTests = (suiteConfig) => {
+  describe('did-jsonld-production', () => {
+    suiteConfig.dids.forEach((did) => {
+      describe(did, () => {
+        for(const [mediaType, resolutionResult] of Object.entries(suiteConfig[did])) {
+          if(mediaType === 'application/did+ld+json') {
+            generateJsonldProductionTests({did, resolutionResult});
+          }
+        }
+      });
+    });
+  });
+};
+
+module.exports = { didJsonldProductionTests };
---
+++ packages/did-core-test-server/suites/did-production/did-producer.js
@@ -0,0 +1,70 @@
+const utils = require('./utils');
+const jsonMediaTypes = ['application/did+ld+json', 'application/did+json'];
+const deepEqual = require('deep-equal')
+
+const generateDidProducerTests = ({did, resolutionResult}) => {
+  const {didDocument} = resolutionResult;
+  const contentType = resolutionResult.didResolutionMetadata['contentType'];
+
+  it('6.1 Production and Consumption - A conforming producer MUST take a ' +
+    'DID document data model and a representation-specific entries map as ' +
+    'input into the production process. The conforming producer MAY accept ' +
+    'additional options as input into the production process.', async () => {
+      if(contentType === 'application/did+ld+json') {
+        const context = didDocument['@context'];
+        if(typeof context === 'string') {
+          expect(context).toBe('https://www.w3.org/ns/did/v1');
+        } else if(Array.isArray(context)) {
+          expect(context[0]).toBe('https://www.w3.org/ns/did/v1');
+          reserializedContext = JSON.parse(JSON.stringify(context));
+          expect(deepEqual(context, reserializedContext)).toBe(true);
+        } else {
+          throw new Error('Invalid @context value '+ context);
+        }
+      }
+  });
+
+  it('6.1 Production and Consumption - A conforming producer MUST ' +
+    'serialize all entries in the DID document data model, and the ' +
+    'representation-specific entries map, that do not have explicit ' +
+    'processing rules for the representation being produced using only the ' +
+    'representation\'s data type processing rules and return the ' +
+    'serialization after the production process completes.', async () => {
+      if(jsonMediaTypes.includes(contentType)) {
+        reserializedDidDocument = JSON.parse(JSON.stringify(didDocument));
+        expect(deepEqual(didDocument, reserializedDidDocument)).toBe(true);
+      } else {
+        throw new Error('Unknown producer for content-type: '+ contentType);
+      }
+  });
+
+  it('6.1 Production and Consumption - A conforming producer MUST return ' +
+    'the Media Type string associated with the representation after the ' +
+    'production process completes.', async () => {
+      expect(typeof contentType === 'string').toBe(true);
+  });
+
+  it('6.1 Production and Consumption - A conforming producer MUST NOT ' +
+    'produce non-conforming DIDs or DID documents.', async () => {
+      if(jsonMediaTypes.includes(contentType)) {
+        reserializedDidDocument = JSON.parse(JSON.stringify(didDocument));
+        expect(deepEqual(didDocument, reserializedDidDocument)).toBe(true);
+      } else {
+        throw new Error('Unknown producer for content-type: '+ contentType);
+      }
+  });
+}
+
+const didProducerTests = (suiteConfig) => {
+  describe('did-producer', () => {
+    suiteConfig.dids.forEach((did) => {
+      describe(did, () => {
+        for(const [mediaType, resolutionResult] of Object.entries(suiteConfig[did])) {
+          generateDidProducerTests({did, resolutionResult});
+        }
+      });
+    });
+  });
+};
+
+module.exports = { didProducerTests };
