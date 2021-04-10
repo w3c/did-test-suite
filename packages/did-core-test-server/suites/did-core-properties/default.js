1b5f6eca5d1f33866b490390aef37f5fcaedcde4
Manu Sporny <msporny@digitalbazaar.com>
2021-04-10 18:13:06 -0400

    Add did:key (2020 cryptosuites) to implementations.

--- packages/did-core-test-server/suites/did-core-properties/default.js
+++ packages/did-core-test-server/suites/did-core-properties/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "did-spec",
   "didMethods": [
     require('../implementations/did-example-didwg.json'),
-    require('../implementations/did-key-2018-db.json')
+    require('../implementations/did-key-2018-db.json'),
+    require('../implementations/did-key-2020-db.json')
   ]
 }
--- packages/did-core-test-server/suites/did-identifier/default.js
+++ packages/did-core-test-server/suites/did-identifier/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "did-identity",
   "didMethods": [
     require('../implementations/did-example-didwg.json'),
-    require('../implementations/did-key-2018-db.json')
+    require('../implementations/did-key-2018-db.json'),
+    require('../implementations/did-key-2020-db.json')
   ]
 }
