934c4aada52e8c49d5dda0ba4d408d18bf9541be
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-04-15 05:44:56 +0900

    Implementing: toBeBase58String/isBase58String

---
+++ packages/jest-did-matcher/src/matchers/toBeBase58String/index.js
@@ -0,0 +1,31 @@
+import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+import predicate from './predicate';
+
+const passMessage = received => () =>
+    matcherHint('.not.toBeBase58String', 'received', '') +
+    '\n\n' +
+    'Expected value to not be of a Base58 string received:\n' +
+    `  ${printReceived(received)}`;
+
+const failMessage = received => () => 
+    matcherHint('.toBeBase58String', 'received', '') +
+    '\n\n' +
+    'Expected value to be of a Base58 string:\n' +
+    `  ${printExpected('a Base 58 string')}` +
+    'Received:\n' +
+    `  ${printReceived(received)}`;
+
+export default {
+    toBeBase58String: expected => {
+        const pass = predicate(expected);
+        if (pass) {
+            return { pass: true, message: passMessage(expected) };
+        }
+
+        return { pass: false, message: failMessage(expected) };
+    },
+
+    isBase58String: obj => {
+        return predicate(obj);
+    }
+};
