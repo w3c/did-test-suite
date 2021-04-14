5a900027e4e7991757b445fff112c116a22bbee8
Joel Thorstensson <joel@oed.earth>
2021-04-14 10:02:24 +0200

    Add did:3 from 3Box Labs

--- packages/did-core-test-server/suites/did-identifier/default.js
+++ packages/did-core-test-server/suites/did-identifier/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "did-identity",
   "didMethods": [
     require('../implementations/did-example-didwg.json'),
-    require('../implementations/did-key-2018-db.json')
+    require('../implementations/did-key-2018-db.json'),
+    require('../implementations/did-3-2021-3box-labs.json')
   ]
 }
--- packages/did-core-test-server/suites/did-production/default.js
+++ packages/did-core-test-server/suites/did-production/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "did-production",
   "didMethods": [
     require('../implementations/did-example-didwg.json'),
-    require('../implementations/did-key-2018-db.json')
+    require('../implementations/did-key-2018-db.json'),
+    require('../implementations/did-3-2021-3box-labs.json')
   ]
 }
