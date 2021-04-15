a91361dc76a9eac8bd947880b2486c58b7684a2d
Joel Thorstensson <joel@oed.earth>
2021-04-15 15:10:04 +0200

    Add resolver implmeentation for did:3

--- packages/did-core-test-server/suites/did-resolution/default.js
+++ packages/did-core-test-server/suites/did-resolution/default.js
@@ -1,6 +1,7 @@
 module.exports = {
   "name": "7.1 DID Resolution",
   "resolvers": [
-    require('../implementations/resolver-example-didwg.json')
+    require('../implementations/resolver-example-didwg.json'),
+    require('../implementations/resolver-3-3box-labs.json')
   ]
 }
