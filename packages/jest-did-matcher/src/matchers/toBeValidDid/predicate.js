56eed732d49617e5c57d55075420ca1d74d886b0
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-04-15 20:14:00 +0900

    Fixing regexp for toBeValidDid/isValidDid to match with latest spec

--- packages/jest-did-matcher/src/matchers/toBeValidDid/predicate.js
+++ packages/jest-did-matcher/src/matchers/toBeValidDid/predicate.js
@@ -1,4 +1,4 @@
 export default expected => {
-    const didRegex = /^did:(?<method>[a-z0-9]+):(?<idchar>[a-zA-Z0-9:\\-_]+$)/;
+    const didRegex = /^did:(?<method>[a-z0-9]+):(?<idchar>([a-zA-Z0-9.\-_]|%[0-9a-fA-F]{2})+$)/;
     return didRegex.test(expected);
 };
