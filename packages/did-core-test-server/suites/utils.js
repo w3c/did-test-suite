be681fbcd9335ab1a4b2fd50959cf26057c45dea
Shigeya Suzuki <shigeya@wide.ad.jp>
2021-04-15 20:15:20 +0900

    Fixing regexp for util.js:isValidDid
    - Added <pct-encoded> charcters

--- packages/did-core-test-server/suites/utils.js
+++ packages/did-core-test-server/suites/utils.js
@@ -18,7 +18,7 @@ const isValidBase58 = (value) => {
 };
 
 const isValidDID = (did) => {
-  const didRegex = /did:(?<method>[a-z0-9]+):(?<idchar>[a-zA-Z0-9:\\-_]+)/;
+  const didRegex = /did:(?<method>[a-z0-9]+):(?<idchar>([a-zA-Z0-9.\-_]|%[0-9a-fA-F]{2})+)/;
 
   return didRegex.test(did);
 };
