e1100672fe838face8b93bf1b8877f2032e0b7b7
Oliver Terbu <unknown>
2021-04-20 12:12:51 +0200

    added ethr did results

--- packages/did-core-test-server/suites/did-resolution/default.js
+++ packages/did-core-test-server/suites/did-resolution/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "7.1 DID Resolution",
   "resolvers": [
     require('../implementations/resolver-example-didwg.json'),
-    require('../implementations/resolver-3-3box-labs.json')
+    require('../implementations/resolver-3-3box-labs.json'),
+    require('../implementations/resolver-ethr-consensys-mesh.json')
   ]
 }
