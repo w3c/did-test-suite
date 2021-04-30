b3db0d8d3cf2eceb5057ae28836c79434ccc0b32
Manu Sporny <msporny@digitalbazaar.com>
2021-04-30 11:28:30 -0400

    Remove unnecessary DID Syntax tests.

--- packages/did-core-test-server/suites/did-identifier/did-syntax.js
+++ packages/did-core-test-server/suites/did-identifier/did-syntax.js
@@ -8,19 +8,6 @@ const didSyntaxTests = (suiteConfig) => {
           'ABNF Rules.', async () => {
           expect(didExample).toBeValidDid();
         });
-        it('The URI scheme MUST be "did:"', () => {
-          const url = new URL(didExample);
-          expect(url.protocol === 'did:').toBe(true);
-        });
-        it('The DID method name MUST be an ASCII lowercase string.', () => {
-          const method = didExample.split(':')[1];
-          expect(method).toBeAsciiString();
-          expect(method.toLowerCase()).toBe(method);
-        });
-        it('The DID method name MUST NOT be empty.', () => {
-          const method = didExample.split(':')[1];
-          expect(method).not.toBe('');
-        });
       });
     });
   });
