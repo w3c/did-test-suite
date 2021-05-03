293e3e683a7f6a96afad13d41f1eb4035d9d4533
Markus Sabadello <markus@danubetech.com>
2021-05-03 13:04:08 +0200

    Tiny improvement to XML datetime matcher.

--- packages/jest-did-matcher/src/matchers/toBeDidCoreDatetime/predicate.js
+++ packages/jest-did-matcher/src/matchers/toBeDidCoreDatetime/predicate.js
@@ -1,5 +1,5 @@
 export default expected => {
-    const regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
+    const regex = /[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-6][0-9]:[0-6][0-9]Z/
     return regex.test(expected)
 };
 
