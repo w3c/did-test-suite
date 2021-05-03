0322e4c104c7258c38450b01f69b4bb98e0ec62a
Markus Sabadello <markus@danubetech.com>
2021-05-03 18:00:40 +0200

    Remove skipped tests (but keep comments).
    Suggested by @msporny in https://github.com/w3c/did-test-suite/pull/86#discussion_r625178855.

--- packages/did-core-test-server/suites/did-resolution/did-resolution.js
+++ packages/did-core-test-server/suites/did-resolution/did-resolution.js
@@ -267,11 +267,9 @@ const testDidDocumentMetadata = (didDocumentMetadata, didDocument, expectedOutco
         }
       });
       // As discussed on the 2021-04-13 DID WG topic call, the following test can be skipped (see https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-04-13-did-topic)
-      it.skip('A conforming DID Method specification MUST guarantee that each equivalentId value is logically equivalent to the id property value.', () => {
-      });
+      // 'A conforming DID Method specification MUST guarantee that each equivalentId value is logically equivalent to the id property value.'
       // As discussed on the 2021-04-13 DID WG topic call, the following test can be skipped (see https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-04-13-did-topic)
-      it.skip('equivalentId is a much stronger form of equivalence than alsoKnownAs because the equivalence MUST be guaranteed by the governing DID method.', () => {
-      });
+      // 'equivalentId is a much stronger form of equivalence than alsoKnownAs because the equivalence MUST be guaranteed by the governing DID method.'
     }
   });
   describe('canonicalId', () => {
@@ -287,8 +285,7 @@ const testDidDocumentMetadata = (didDocumentMetadata, didDocument, expectedOutco
         }
       });
       // As discussed on the 2021-04-13 DID WG topic call, the following test can be skipped (see https://www.w3.org/2019/did-wg/Meetings/Minutes/2021-04-13-did-topic)
-      it.skip('A conforming DID Method specification MUST guarantee that the canonicalId value is logically equivalent to the id property value.', () => {
-      });
+      // 'A conforming DID Method specification MUST guarantee that the canonicalId value is logically equivalent to the id property value.'
     }
   });
 };
