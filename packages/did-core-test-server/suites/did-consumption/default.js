4859a749d35f0de81944c3df1ab877502e594d3a
SondreB <sondre@outlook.com>
2021-05-07 20:26:42 +0200

    Fix the implementation for did-consumption and did-production
    - Our implementation has been updated to use correct name of "contentType", and dropped the 'context' since we only support application/did+json.
    - This concludes the first part of our DID Method implementation, the resolution will be added in another PR.

--- packages/did-core-test-server/suites/did-consumption/default.js
+++ packages/did-core-test-server/suites/did-consumption/default.js
@@ -2,6 +2,7 @@ module.exports = {
   "name": "did-consumption",
   "didMethods": [
     require('../implementations/did-example-didwg.json'),
+    require('../implementations/did-is.json'),
     require('../implementations/did-key-2018-db.json'),
     require('../implementations/did-key-2020-db.json'),
     require('../implementations/did-3-2021-3box-labs.json'),
