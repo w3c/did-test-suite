2983e263388dcd3963cc32a6bf9d78f2568a7591
Antonio Antonino <antonio@kilt.io>
2021-05-10 13:44:54 +0200

    feat: add description of light and full Kilt DIDs.

--- packages/did-core-test-server/suites/did-production/default.js
+++ packages/did-core-test-server/suites/did-production/default.js
@@ -4,6 +4,7 @@ module.exports = {
     require('../implementations/did-example-didwg.json'),
     require('../implementations/did-key-2018-db.json'),
     require('../implementations/did-key-2020-db.json'),
+    require('../implementations/did-kilt-2021.json'),
     require('../implementations/did-3-2021-3box-labs.json'),
     require('../implementations/did-ethr-2021-consensys-mesh.json')
   ]
