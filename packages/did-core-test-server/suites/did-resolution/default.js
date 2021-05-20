c76e8f083e9d4168f581f6ab92ac381cd16cb55e
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-05-20 14:55:28 +0200

    Disable resolver-ethr-consensys-mesh.json

--- packages/did-core-test-server/suites/did-resolution/default.js
+++ packages/did-core-test-server/suites/did-resolution/default.js
@@ -2,7 +2,7 @@ module.exports = {
   name: '7.1 DID Resolution',
   resolvers: [
     require('../implementations/resolver-3-3box-labs.json'),
-    require('../implementations/resolver-ethr-consensys-mesh.json'),
+    // require('../implementations/resolver-ethr-consensys-mesh.json'),
     require('../implementations/resolver-example-didwg.json'),
     require('../implementations/universal-resolver-did-ace.json'),
     require('../implementations/universal-resolver-did-bba.json'),
