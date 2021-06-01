886cfe6b6ffe7268ee9919ab82cb824f2a97cf4a
Daniel Buchner <danieljb2@gmail.com>
2021-06-01 07:28:00 -0700

    Add did:ion resolver.

--- packages/did-core-test-server/suites/did-resolution/default.js
+++ packages/did-core-test-server/suites/did-resolution/default.js
@@ -36,6 +36,7 @@ module.exports = {
     require('../implementations/universal-resolver-did-vaa.json'),
     require('../implementations/universal-resolver-did-web.json'),
     require('../implementations/universal-resolver-did-work.json'),
+    require('../implementations/resolver-did-ion.json'),
     require('../implementations/resolver-nft-3box-labs.json'),
     require('../implementations/resolver-did-orb.json')
   ],
