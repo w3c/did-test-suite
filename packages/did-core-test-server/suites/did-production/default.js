662c99bdc2b02e9c45041839e3bbbc7e662ccba4
Charles E. Lehner <charles.lehner@spruceid.com>
2021-07-14 14:20:21 -0400

    Add Spruce implementations

--- packages/did-core-test-server/suites/did-production/default.js
+++ packages/did-core-test-server/suites/did-production/default.js
@@ -30,6 +30,12 @@ module.exports = {
     require('../implementations/did-sov-mattr.json'),
     require('../implementations/did-web-mattr.json'),
     require('../implementations/did-polygon-ayanworks.json'),
+    require('../implementations/did-key-spruce.json'),
+    require('../implementations/did-web-spruce.json'),
+    require('../implementations/did-tz-spruce.json'),
+    require('../implementations/did-onion-spruce.json'),
+    require('../implementations/did-pkh-spruce.json'),
+    require('../implementations/did-webkey-spruce.json'),
     ...brokenFixtures
   ],
 };
