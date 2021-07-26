b028f537f27f53acd90342fd3a70581ae95b45d0
Charles E. Lehner <charles.lehner@spruceid.com>
2021-07-26 12:54:58 -0400

    Add did:ssb implementation (#195)

--- packages/did-core-test-server/suites/did-identifier/default.js
+++ packages/did-core-test-server/suites/did-identifier/default.js
@@ -40,6 +40,7 @@ module.exports = {
     require('../implementations/did-webkey-spruce.json'),
     require('../implementations/did-web-evernym.json'),
     require('../implementations/did-ebsi.json'),
+    require('../implementations/did-ssb.json'),
    ...brokenFixtures
   ],
 };
